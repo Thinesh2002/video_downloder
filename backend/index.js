25891f4e3b8668be1c9e31b770ea75e42c092827
Thineshkaran <sthinesh0730@gmail.com>
2026-03-11 14:01:03 +0530

    c

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(cors({
   origin: [
     "https://video.teckvora.com",
-    "http://localhost:4001"
+    "http://localhost:5173"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
