4e34db2db24b5b888f9f4d47c2af954769f44acd
Thineshkaran <sthinesh0730@gmail.com>
2026-03-11 12:44:22 +0530

    s

--- frontend/src/config/api.jsx
+++ frontend/src/config/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:4001";
+export const API_BASE_URL = "http://video-api.teckvora.com";
 
 const API = axios.create({
   baseURL: `${API_BASE_URL}/api`,
